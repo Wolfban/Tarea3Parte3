705363652523928b360a2d2d301c3b202f4c72e0
Wolfban <estebanbarrera104@gmail.com>
2022-08-21 17:24:06 -0600

    Se agrega el codigo de las acciones de la persona

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@
  */
 public class Main {
     public static void main(String[] args){
+        Persona p = new Persona();
 
+        p.caminar();
     }
 }
--- src/Persona.java
+++ src/Persona.java
@@ -2,20 +2,21 @@
  * @author Esteban Barrera
  */
 public class Persona implements Fursuiter{
-
+    public Persona(){}
 
     public void abrazar() {
+        System.out.println("El fursuiter abraza");
     }
 
 
 
     public void caminar() {
-
+        System.out.println("El fursuiter camina");
     }
 
 
 
     public void vestirse() {
-
+        System.out.println("Se pone su fursuit");
     }
 }
